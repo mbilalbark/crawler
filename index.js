52af2b0fb8e6bf94e54109605df6a90d7afdf3a2
mbilalbark <muh.bark@gmail.com>
2018-10-18 08:19:50 +0300

    Add: Crawler Url Change

--- index.js
+++ index.js
@@ -2,15 +2,23 @@ var HTMLParser = require('node-html-parser');
 var validator = require("email-validator");
 var request = require('request');
 
+let url="https://play.google.com/store/apps/details?id=com.pomelogames.TowerGame";
+let count = 0;
+while(count<2){
+    request(url, function (error, response, body) {
+        var a;
+        var root = HTMLParser.parse(body);
+        a = root.structuredText.split(/\n/);
+       for(i=0;i<a.length;i++){
+            if(validator.validate(a[i]))
+                console.log(a[i]);
+       }
+       // TODO: rawAttrs will parse
+       console.log(root.querySelectorAll("a")[0].rawAttrs)
+    });
+   count++;
+   url="https://play.google.com/store/apps/details?id=com.n2games.westrunner.wildwestredemption";
+}
 
-request('https://stackabuse.com/learn-node-js-a-beginners-guide/', function (error, response, body) {
-    var a;
-    var root = HTMLParser.parse(body);
-    a = root.structuredText.split(/\n/);
-   for(i=0;i<a.length;i++){
-        if(validator.validate(a[i]))
-            console.log(a[i]);
-   }
-});
 
 
